4ec3c4d69d40b22e965c8cb09231b19f02200af8
BALAVARDHAN REDDY <balavardhan.k21@iiits.in>
2023-11-09 12:39:14 +0530

    Truecaller

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,5 +1,4 @@
 import abi from './Truecaller.json';
 
 export const contractABI = abi.abi;
-export const contract_Address = '0xFa95Bb79279452ca56A181A64233291e75c08520';
-
+export const contract_Address = '0x7cAE9C916A22B26afC64d1123430D7703DbCcfe5';
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -3,12 +3,15 @@
 require('@nomiclabs/hardhat-waffle');
 
 /** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+
 module.exports = {
   solidity: "0.8.0",
   networks: {
-    Sepolia:{
-      url:'https://eth-sepolia.g.alchemy.com/v2/29ZYauhqr1hecI8ROOH2Ajtcc0kqgBaR',
-      accounts:['e2cbd0a4a786f1d2b1ee321fef781c3be67ad62c4f1030fc9a0b64f654b76017']
-    }
-  }
-}
+    Sepolia: {
+      url: process.env.ALCHEMY_URL,
+      accounts: [process.env.ALCHEMY_PRIVATE_KEY],
+    },
+  },
+};
+
