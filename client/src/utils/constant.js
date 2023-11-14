679b22393686c308631dc613d81f3bb9e8f45e59
BALAVARDHAN REDDY <balavardhan.k21@iiits.in>
2023-11-14 18:59:53 +0530

    True_Caller

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,4 +1,4 @@
 import abi from './Truecaller.json';
 
 export const contractABI = abi.abi;
-export const contract_Address = '0x7cAE9C916A22B26afC64d1123430D7703DbCcfe5';
+export const contract_Address = '0xD9b6EC39556507c3807D8A2fb77572d0394f919B';
